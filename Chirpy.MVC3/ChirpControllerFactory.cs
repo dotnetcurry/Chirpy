4f86e5d722ddc7ea1dcfa83227b7388510f1c1cf
Richard Fawcett <r_d_fawcett@hotmail.com>
2012-05-09 23:59:57 +0100

    Split MVC project into two, the existing (now in renamed folder Chirpy.MVC4) and a new one in Chirpy.MVC3. Made changes to MVC3 project to support MVC3, and remove Razor / Web Pages 2 syntax (such as @Html.DisplayNameFor). MVC3 project should now work in a standard installation of MVC3. Two solution files now exist, one for each of the two flavors of MVC.

---
+++ Chirpy.MVC3/ChirpControllerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Chirpy.Domain.Repository;
+using Chirpy.Controllers;
+
+namespace Chirpy
+{
+    
+    public class ChirpControllerFactory : DefaultControllerFactory
+    {
+        private readonly Dictionary<string, Func<RequestContext, IController>> _controllerMap;
+
+        public ChirpControllerFactory(IChirpRepository chirpRepository)
+        {
+            if (chirpRepository == null)
+            {
+                throw new ArgumentNullException("documentStore");
+            }
+            this._controllerMap = new Dictionary<string, Func<RequestContext, IController>>();
+            this._controllerMap["Home"] = context => new HomeController(chirpRepository);
+            this._controllerMap["Administration"] = context => new AdministrationController(chirpRepository);
+        }
+
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            if (this._controllerMap.ContainsKey(controllerName))
+            {
+                return this._controllerMap[controllerName](requestContext);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            base.ReleaseController(controller);
+        }
+    }
+}
